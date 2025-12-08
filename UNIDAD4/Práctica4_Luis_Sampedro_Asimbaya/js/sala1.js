4f9321849e83a8d5fd76161a690f9d66cb265ffd
Luisfer <lfernans0210@gmail.com>
2025-12-08 18:10:06 +0100

    Agregación de bloque de preguntas en html y función de iniciar quiz clonado en js para mostrar dicho bloque y esconder el de inicio

--- UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala1.js
+++ UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala1.js
@@ -1,13 +1,13 @@
 const puntuacionObjetivo = Symbol("Puntuación necesaria para pasar a la siguiente sala")
 
-const quiz1 = {
+const quiz = {
     titulo: "Quiz sala 1",
     dificultad: "Baja",
     tematica: {
         capitales : {
             preguntas: [
                 {p: "¿Cuál es la capital de España?", r: "Madrid"},
-                {p: "¿Cuál es la capital de Francia?", r: "Paris"},
+                {p: "¿Cuál es la capital de Francia?", r: "París"},
                 {p: "¿Cuál es la capital de Italia?", r: "Roma"},
                 {p: "¿Cuál es la capital de Alemania?", r: "Berlín" },
                 {p: "¿Cuál es la capital de Portugal?", r: "Lisboa" },
@@ -18,7 +18,7 @@ const quiz1 = {
             preguntas : [
                 {p: "¿Cuánto es 10 + 15 + 20?", r: 45},
                 {p: "¿Cuánto es 7 x 8?", r: 56},
-                {p: "¿Cuánto es 9/3?", r: 3},
+                {p: "¿Cuánto es 9 ÷ 3?", r: 3},
                 {p: "¿Cuánto es 12 + 18?", r: 30 },
                 {p: "¿Cuánto es 6 x 9?", r: 54 },
                 {p: "¿Cuánto es 25 - 7?", r: 18 }
@@ -26,11 +26,11 @@ const quiz1 = {
         },
         cultura: {
             preguntas: [
-                {p: "¿A qué temperatura hierve el agua (en grados Celsius)?", r: 100},
+                {p: "¿A qué temperatura, en grados Celsius, hierve el agua?", r: 100},
                 {p: "¿Cuál es el planeta más grande de nuestro sistema solar?", r: "Júpiter"},
-                {p: "¿En qué año se acabí la segunda guerra mundial?", r: 1945},
+                {p: "¿En qué año se acabó la segunda guerra mundial?", r: 1945},
                 {p: "¿Cuál es el océano más grande del mundo?", r: "Pacífico" },
-                {p: "¿Qué gas necesitan las plantas para hacer la fotosíntesis? (Fórmula)", r: "CO2"},
+                {p: "Fórmula del gas que necesitan las plantas para realizar la fotosíntesis", r: "CO2"},
                 {p: "¿En qué año llegó el ser humano a la Luna?", r: 1969 }
             ]
         }
@@ -42,28 +42,44 @@ const quiz1 = {
     [puntuacionObjetivo] : 60
 };
 
+let contador = 1
+let titulo = ""
+
 // funciones de boton tematica
 function preguntarCapitales() {
-    if (nombre.value) {
-        let capitalesClon = structuredClone(quiz1.tematica.capitales); // clonamos unicamente la seccion de preguntas de capitales
+    if (nombre.value.trim()) {
+        let capitalesClon = structuredClone(quiz.tematica.capitales); // clonamos unicamente la seccion de preguntas de capitales
+        iniciarQuizClonado(capitalesClon)
+        titulo = "Capitales";
     } else {
         alert('Debe introducir un nombre para comenzar el juego');
     }
 }
 
 function preguntarMatematicas() {
-    if (nombre.value) {
-        let matematicasClon = structuredClone(quiz1.tematica.matematicas); // clonamos unicamente la seccion de preguntas de matematicas
+    if (nombre.value.trim()) {
+        let matematicasClon = structuredClone(quiz.tematica.matematicas); // clonamos unicamente la seccion de preguntas de matematicas
+        iniciarQuizClonado(matematicasClon)
+        titulo = "Matemáticas";
     } else {
         alert('Debe introducir un nombre para comenzar el juego');
     }
 }
 
 function preguntarCultura() {
-    if (nombre.value) {
-        let culturaClon = structuredClone(quiz1.tematica.cultura); // clonamos unicamente la seccion de preguntas de matematicas
+    if (nombre.value.trim()) {
+        let culturaClon = structuredClone(quiz.tematica.cultura); // clonamos unicamente la seccion de preguntas de matematicas
+        iniciarQuizClonado(culturaClon)
+        titulo = "Cultura";
     } else {
         alert('Debe introducir un nombre para comenzar el juego');
     }
 }
 
+// funcion para iniciar cuestionario clonado - se oculta menu de inicio y se muestra menu de preguntas
+function iniciarQuizClonado(temaClonado) {
+    document.getElementById('menuInicio').style.display = "none";
+    preguntasActuales = temaClonado.preguntas;
+    indicePregunta = 0;
+    document.getElementById("contenedorPreguntas").style.display = "block";
+}
