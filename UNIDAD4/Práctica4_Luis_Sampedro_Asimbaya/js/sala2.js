370ef903a66aef0cd5dce3edc64cdabb22e3d59f
Luisfer <lfernans0210@gmail.com>
2025-12-15 17:24:51 +0100

    Práctica Unidad 4 (segunda entrega): Implementación de métodos de arrays como indexOf para encontrar en el array la palabra del usuario, splice para borrar esta misma palabra dentro del array e implementación de validaciones en el input texto.

--- UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala2.js
+++ UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala2.js
@@ -1,8 +1,39 @@
+// array que el usuario tendrá que ordenar
 let marcasCoches = ["Toyota", "Volskwagen", "Honda", "Renault", "Seat", "Suzuki",
     "Ferrari", "Opel", "Fiat", "Dacia", "Kia", "Ford", "Peugeot", "Lexus", "Audi"]
 
-parrafoEnunciado = document.getElementById('arrayOrdenar');
+let parrafoEnunciado = document.getElementById('arrayOrdenar');
 
 for (let marca of marcasCoches) {
     parrafoEnunciado.innerHTML += `[${marca}] `;
+}
+
+let ordenUsuario = [];
+
+function enviarRespuesta() {
+    //guardamos en una variable la respuesta del usuario
+    let marcaUsuario = respuestaUsuario.value.trim()
+    if (marcaUsuario) {
+        // vaciamos el input del usuario
+        document.getElementById('respuestaUsuario').value = "";
+        // reiniciamos el parrafo que muestra el array para evitar conflicto a la hora de modificar este output
+        parrafoEnunciado.innerHTML = "";
+        if (marcasCoches.includes(marcaUsuario)) {
+            let indice = marcasCoches.indexOf(marcaUsuario);
+            // borramos un elemento del indice extraido de la palabra del usuario
+            marcasCoches.splice(indice, 1);
+            for (let marca of marcasCoches) {
+                parrafoEnunciado.innerHTML += `[${marca}] `;
+            }
+        } else {
+            alert('La palabra introducida no se encuentra dentro de la lista')
+            for (let marca of marcasCoches) {
+                parrafoEnunciado.innerHTML += `[${marca}] `;
+            }
+        }
+        // vamos guardando en un array las respuestas del usuario
+        ordenUsuario.push(marcaUsuario);
+    } else {
+        alert('Debes introducir una marca en el campo')
+    }
 }
