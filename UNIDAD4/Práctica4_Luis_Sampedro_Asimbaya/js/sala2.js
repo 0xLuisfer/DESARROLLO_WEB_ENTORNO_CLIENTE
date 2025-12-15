0387dcbce57bdc8fe7f0141f60d80576aa0de751
Luisfer <lfernans0210@gmail.com>
2025-12-15 18:38:24 +0100

    Práctica Unidad 4 (segunda entrega): Creación de nuevo array con los métodos slice() y sort() para ordenarlo alfabéticamente, uso de función compararArrays() utilizando el método join() para comparar el orden del nuevo array con el orden introducido por el usuario y preparación de la segunda parte del juego.

--- UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala2.js
+++ UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala2.js
@@ -12,28 +12,64 @@ let ordenUsuario = [];
 
 function enviarRespuesta() {
     //guardamos en una variable la respuesta del usuario
-    let marcaUsuario = respuestaUsuario.value.trim()
-    if (marcaUsuario) {
-        // vaciamos el input del usuario
-        document.getElementById('respuestaUsuario').value = "";
-        // reiniciamos el parrafo que muestra el array para evitar conflicto a la hora de modificar este output
-        parrafoEnunciado.innerHTML = "";
-        if (marcasCoches.includes(marcaUsuario)) {
-            let indice = marcasCoches.indexOf(marcaUsuario);
-            // borramos un elemento del indice extraido de la palabra del usuario
-            marcasCoches.splice(indice, 1);
-            for (let marca of marcasCoches) {
-                parrafoEnunciado.innerHTML += `[${marca}] `;
+    let marcaUsuario = respuestaUsuario.value.trim();
+        if (marcaUsuario) {
+            // vaciamos el input del usuario
+            document.getElementById('respuestaUsuario').value = "";
+            // reiniciamos el parrafo que muestra el array para evitar conflicto a la hora de modificar este output
+            parrafoEnunciado.innerHTML = "";
+            if (marcasCoches.includes(marcaUsuario)) {
+                let indice = marcasCoches.indexOf(marcaUsuario);
+                // borramos un elemento del indice extraido de la palabra del usuario
+                marcasCoches.splice(indice, 1);
+                for (let marca of marcasCoches) {
+                    parrafoEnunciado.innerHTML += `[${marca}] `;
+                }
+            } else {
+                alert('La palabra introducida no se encuentra dentro de la lista')
+                for (let marca of marcasCoches) {
+                    parrafoEnunciado.innerHTML += `[${marca}] `;
+                }
+            }
+            // guardamos en un array nuevo las palabras introducidas por el usuario
+            ordenUsuario.push(marcaUsuario);
+            // hacemos llamada a la funcion cuando no queden elementos en el array original
+            if (marcasCoches.length == 0) {
+                    compararArrays();
             }
         } else {
-            alert('La palabra introducida no se encuentra dentro de la lista')
-            for (let marca of marcasCoches) {
-                parrafoEnunciado.innerHTML += `[${marca}] `;
-            }
+            alert('Debes introducir una marca en el campo')
         }
-        // vamos guardando en un array las respuestas del usuario
-        ordenUsuario.push(marcaUsuario);
+}
+
+// creamos un array nuevo pero ordenado alfabeticamente
+let marcasOrdenadas = marcasCoches.slice().sort();
+
+
+// comparamos ambos arrays
+function compararArrays() {
+    if (marcasOrdenadas.join().toLowerCase() == ordenUsuario.join().toLowerCase()) {
+        document.getElementById('titulo').style.color = "green";
+        document.getElementById('titulo').innerHTML = `¡Felicidades! Has ordenado todos los elementos correctamente.`
+        parrafoEnunciado.innerHTML = `Has logrado pasar a la siguiente parte del reto<br><button onclick='segundoReto()'>Segunda parte</button>`;
+        // escondemos input y botón de respuesta
+        document.getElementById('botonUsuario').style.display = 'none';
+        document.getElementById('respuestaUsuario').style.display = 'none';
     } else {
-        alert('Debes introducir una marca en el campo')
+        document.getElementById('titulo').style.color = "red";
+        document.getElementById('titulo').innerHTML = `Orden incorrecto, vuelve a intentarlo nuevamente.<br><button onclick='recargarPagina()'>Intentar de nuevo</button>`;
+        // escondemos input y botón de respuesta
+        document.getElementById('botonUsuario').style.display = 'none';
+        document.getElementById('respuestaUsuario').style.display = 'none';
     }
+}
+
+// si el usuario falla tiene un botón para intentarlo nuevamente
+function recargarPagina() {
+    window.location.reload()
+}
+
+function segundoReto() {
+    document.getElementById('contenedorPrimerJuego').style.display = "none";
+    document.getElementById('contenedorSegundoJuego').style.display = "block";
 }
