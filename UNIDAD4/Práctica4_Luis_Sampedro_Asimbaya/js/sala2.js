d63aa5ed002ad0178df76170d35edbbe5cedb034
Luisfer <lfernans0210@gmail.com>
2025-12-16 19:59:05 +0100

    Práctica Unidad 4 (segunda entrega): Arreglos en el primer reto para permitir tanto mayúsculas como minúsculas en el input y cambios en algunos estilos y textos del menú

--- UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala2.js
+++ UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala2.js
@@ -1,18 +1,19 @@
 // array que el usuario tendrá que ordenar
-let marcasCoches = ["Toyota", "Volskwagen", "Honda", "Renault", "Seat", "Suzuki",
-    "Ferrari", "Opel", "Fiat", "Dacia", "Kia", "Ford", "Peugeot", "Lexus", "Audi"]
+let marcasCoches = ["TOYOTA", "VOLSKWAGEN", "HONDA", "RENAULT", "SEAT", "SUZUKI",
+    "FERRARI", "OPEL", "FIAT", "DACIA", "KIA", "FORD", "PEUGEOT", "LEXUS", "AUDI"]
 
 let parrafoEnunciado = document.getElementById('arrayOrdenar');
 
 for (let marca of marcasCoches) {
-    parrafoEnunciado.innerHTML += `  [${marca}]  `;
+    parrafoEnunciado.style.fontSize = `15px`;
+    parrafoEnunciado.innerHTML += `${marca}<br>`;
 }
 
 let ordenUsuario = [];
 
 function enviarRespuesta() {
     //guardamos en una variable la respuesta del usuario
-    let marcaUsuario = respuestaUsuario.value.trim();
+    let marcaUsuario = respuestaUsuario.value.trim().toUpperCase();
         if (marcaUsuario) {
             // vaciamos el input del usuario
             document.getElementById('respuestaUsuario').value = "";
@@ -23,14 +24,16 @@ function enviarRespuesta() {
                 // borramos un elemento del indice extraido de la palabra del usuario
                 marcasCoches.splice(indice, 1);
                 for (let marca of marcasCoches) {
-                    parrafoEnunciado.innerHTML += `[${marca}] `;
+                    parrafoEnunciado.style.fontSize = `15px`;
+                    parrafoEnunciado.innerHTML += `${marca}<br>`;
                 }
                 // guardamos en un array nuevo las palabras introducidas por el usuario
                 ordenUsuario.push(marcaUsuario);
             } else {
                 alert('La palabra introducida no se encuentra dentro de la lista')
                 for (let marca of marcasCoches) {
-                    parrafoEnunciado.innerHTML += `[${marca}] `;
+                    parrafoEnunciado.style.fontSize = `15px`;
+                    parrafoEnunciado.innerHTML += `${marca}<br>`;
                 }
             }
             // hacemos llamada a la funcion cuando no queden elementos en el array original
@@ -51,7 +54,7 @@ function compararArrays() {
     if (marcasOrdenadas.join().toLowerCase() == ordenUsuario.join().toLowerCase()) {
         document.getElementById('titulo').style.color = "green";
         document.getElementById('titulo').innerHTML = `¡Felicidades! Has ordenado todos los elementos correctamente.`
-        parrafoEnunciado.innerHTML = `Has logrado pasar a la siguiente parte del reto<br><button onclick='segundoReto()'>Segunda parte</button>`;
+        parrafoEnunciado.innerHTML = `Has logrado pasar a la segunda parte de este reto<br><button onclick='segundoReto()'>Segunda parte</button>`;
         // escondemos input y botón de respuesta
         document.getElementById('botonUsuario').style.display = 'none';
         document.getElementById('respuestaUsuario').style.display = 'none';
@@ -93,31 +96,32 @@ function mostrarPistaPais() {
     // desestructuración del primer elemento de arrayPistas [país, japón]
     let [clave, valor] = arrayPistas[0];
     document.getElementById('boton1').style.backgroundColor = 'white';
-    document.getElementById('pista').innerHTML = `<p>PISTA 1: El ${clave} de este vehículo es ${valor}<p>`;
+    // se muestra un texto que contiene clave, valor del elemento del array correspondiente
+    document.getElementById('pista').innerHTML = `PISTA 1: El ${clave} de origen de esta marca es ${valor}`;
 }
 
 function mostrarPistaVocal() {
     let [clave, valor] = arrayPistas[1];
     document.getElementById('boton2').style.backgroundColor = 'white';
-    document.getElementById('pista').innerHTML = `<p>PISTA 2: La marca contiene ${valor} ${clave}<p>`;
+    document.getElementById('pista').innerHTML = `PISTA 2: El nombre de la marca contiene ${valor} ${clave}`;
 }
 
 function mostrarPistaAño() {
     let [clave, valor] = arrayPistas[2];
     document.getElementById('boton3').style.backgroundColor = 'white';
-    document.getElementById('pista').innerHTML = `<p>PISTA 3: El ${clave} de creación de la marca fue en ${valor}<p>`;
+    document.getElementById('pista').innerHTML = `PISTA 3: El ${clave} de creación de la marca fue en ${valor}`;
 }
 
 function mostrarPistaModelo() {
     let [clave, valor] = arrayPistas[3];
     document.getElementById('boton4').style.backgroundColor = 'white';
-    document.getElementById('pista').innerHTML = `<p>PISTA 4: ${valor} es un ${clave} de esta marca<p>`;
+    document.getElementById('pista').innerHTML = `PISTA 4: ${valor} es un ${clave} de esta marca`;
 }
 
 function mostrarPistaCaracteristica() {
     let [clave, valor] = arrayPistas[4];
     document.getElementById('boton5').style.backgroundColor = 'white';
-    document.getElementById('pista').innerHTML = `<p>PISTA 5: Una ${clave} de este vehículo es que es muy ${valor}<p>`;
+    document.getElementById('pista').innerHTML = `PISTA 5: Una ${clave} de estos coches es que son muy ${valor}s`;
 }
 
 function usuarioAdivina() {
@@ -125,8 +129,13 @@ function usuarioAdivina() {
     if (inputUsuario) {
         if (inputUsuario.toLowerCase() == "honda") {
             document.getElementById('pista').style.color = `green`;
-            document.getElementById('pista').innerHTML = `<h3>¡CORRECTO! Has podido arrancar el coche<h3>`;
+            document.getElementById('pista').innerHTML = `¡CORRECTO! Has podido arrancar el coche`;
             document.getElementById('botonesAdivinanza').style.display = `none`;
+            document.getElementById('adivinanzaUsuario').style.display = `none`;
+            document.getElementById('botonEnviar').style.display = `none`;
+        } else {
+            alert('Respuesta incorrecta, volverás al último punto de control');
+            window.location.reload();
         }
     } else {
         alert('Debes introducir una respuesta en el campo para continuar')
