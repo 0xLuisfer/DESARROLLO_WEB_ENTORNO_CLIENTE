9a22a779dc52bc5c832bde41859b50361f5a0d2f
Luisfer <lfernans0210@gmail.com>
2025-12-16 17:44:01 +0100

    Práctica Unidad 4 (segunda entrega): Implementación de botones para ir desplegando las pistas del reto haciendo uso de la desetructuración mostrando un texto formateado con la clave y el valor del array. Se añade también función para comparar el input del usuario con la palabra correcta

--- UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala2.js
+++ UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala2.js
@@ -5,7 +5,7 @@ let marcasCoches = ["Toyota", "Volskwagen", "Honda", "Renault", "Seat", "Suzuki"
 let parrafoEnunciado = document.getElementById('arrayOrdenar');
 
 for (let marca of marcasCoches) {
-    parrafoEnunciado.innerHTML += `[${marca}] `;
+    parrafoEnunciado.innerHTML += `  [${marca}]  `;
 }
 
 let ordenUsuario = [];
@@ -80,32 +80,56 @@ function segundoReto() {
 let pistasMap = new Map();
 
 // objeto de pistas que pasaremos por parámetro
-let pistasHonda = {pais: "Japón", vocales: 2, año: 1948, modelo: "Accord", característica: "Fiable"};
+let pistasHonda = {país: "Japón", vocales: 2, año: 1948, modelo: "Accord", característica: "fiable"};
 
 // introducimos clave "Honda" (por si creamos pistas para otros coches) y el objeto de pistasHonda como parámetros dentro de nuesto pistasMap
 pistasMap.set("Honda", pistasHonda);
 
-// convertimos en un array clave,valor con Object.entries y metememos por parámetro el objeto que hemos creado de pistas
+// convertimos en un array clave,valor con Object.entries y metememos por parámetro el objeto que hemos creado de pistas, lo guardamos en una variable
 let arrayPistas = Object.entries(pistasMap.get('Honda'));
 
 
-function desplegarPista1() {
-    
+function mostrarPistaPais() {
+    // desestructuración del primer elemento de arrayPistas [país, japón]
+    let [clave, valor] = arrayPistas[0];
+    document.getElementById('boton1').style.backgroundColor = 'white';
+    document.getElementById('pista').innerHTML = `<p>PISTA 1: El ${clave} de este vehículo es ${valor}<p>`;
 }
 
-function desplegarPista1() {
-    
+function mostrarPistaVocal() {
+    let [clave, valor] = arrayPistas[1];
+    document.getElementById('boton2').style.backgroundColor = 'white';
+    document.getElementById('pista').innerHTML = `<p>PISTA 2: La marca contiene ${valor} ${clave}<p>`;
 }
 
-function desplegarPista1() {
-    
+function mostrarPistaAño() {
+    let [clave, valor] = arrayPistas[2];
+    document.getElementById('boton3').style.backgroundColor = 'white';
+    document.getElementById('pista').innerHTML = `<p>PISTA 3: El ${clave} de creación de la marca fue en ${valor}<p>`;
 }
 
-function desplegarPista1() {
-    
+function mostrarPistaModelo() {
+    let [clave, valor] = arrayPistas[3];
+    document.getElementById('boton4').style.backgroundColor = 'white';
+    document.getElementById('pista').innerHTML = `<p>PISTA 4: ${valor} es un ${clave} de esta marca<p>`;
 }
 
-function desplegarPista1() {
-    
+function mostrarPistaCaracteristica() {
+    let [clave, valor] = arrayPistas[4];
+    document.getElementById('boton5').style.backgroundColor = 'white';
+    document.getElementById('pista').innerHTML = `<p>PISTA 5: Una ${clave} de este vehículo es que es muy ${valor}<p>`;
+}
+
+function usuarioAdivina() {
+    let inputUsuario = adivinanzaUsuario.value.trim();
+    if (inputUsuario) {
+        if (inputUsuario.toLowerCase() == "honda") {
+            document.getElementById('pista').style.color = `green`;
+            document.getElementById('pista').innerHTML = `<h3>¡CORRECTO! Has podido arrancar el coche<h3>`;
+            document.getElementById('botonesAdivinanza').style.display = `none`;
+        }
+    } else {
+        alert('Debes introducir una respuesta en el campo para continuar')
+    }
 }
 
