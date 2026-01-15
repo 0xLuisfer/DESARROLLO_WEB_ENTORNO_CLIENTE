80ba49e0aa625be407d3395936ff6cf26954fad7
Luisfer <lfernans0210@gmail.com>
2026-01-15 20:21:58 +0100

    Práctica Unidad 4 (tercera entrega): Creación de funciones onclick para la generación de distintos personajes heredados de la clase Héroe, e implementación de manejo de errores (try...catch) para evitar que el usuario cree un personaje sin introducir un nombre antes.

--- UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala3.js
+++ UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala3.js
@@ -3,7 +3,50 @@ import {Luchador} from './Luchador.js';
 import {Curandero} from './Curandero.js';
 import {Tanque} from './Tanque.js';
 
-let luchador1 = new Luchador('Pepe');
+let nombre = document.getElementById('nombrePersonaje');
+
+window.crearCurandero = function () {
+    try {
+        if (!nombre.value.trim()) {
+            // creamos error personalizado
+            throw new Error ("Debes introducir un nombre");
+        }
+        let curandero1 = new Curandero(nombre);
+        alert(curandero1.mostrarVida());
+    } catch(err) {
+            alert(err.message)
+    }
+}
+
+window.crearLuchador = function () {
+    try {
+        if (!nombre.value.trim()) {
+            // creamos error personalizado
+            throw new Error ("Debes introducir un nombre");
+        }
+        let luchador1 = new Luchador(nombre);
+        alert(luchador1.mostrarVida());
+    } catch(err) {
+            alert(err.message)
+    }
+}
+
+window.crearTanque = function () {
+    try {
+        if (!nombre.value.trim()) {
+            // creamos error personalizado
+            throw new Error ("Debes introducir un nombre");
+        }
+        let tanque1 = new Tanque(nombre);
+        alert(tanque1.mostrarVida());
+    } catch(err) {
+            alert(err.message)
+    }
+}
+
+
+
+/* let luchador1 = new Luchador('Pepe');
 luchador1.recibirGolpe();
 alert(luchador1.mostrarVida());
 
@@ -12,4 +55,4 @@ curandero1.curarse();
 alert(curandero1.mostrarVida());
 
 let tanque1 = new Tanque("Pedro");
-alert(tanque1.mostrarVida());
+alert(tanque1.mostrarVida()); */
