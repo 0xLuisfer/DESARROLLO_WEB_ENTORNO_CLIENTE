7c20052d67423b0461f9395f066d9cb4f17ccaa8
Luisfer <lfernans0210@gmail.com>
2026-02-07 20:01:30 +0100

    Práctica Unidad 5: (primera entrega): Se añade cuarto evento donde se manejan varios div anidados para evitar la propagación de eventos, con event.stopPropagation(), cuando el usuario cambia cualquier atributo de alguno de ellos, en este caso el color de fondo de cada contenedor

--- UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala4.js
+++ UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala4.js
@@ -48,3 +48,64 @@ function ctrlResultado(event) {
 
 // disparamos el evento CTRL + Resultado
 document.addEventListener("keydown", ctrlResultado);
+
+//evento cuarto contenedor - validacion de colores evitando propagación
+let colores = ["black", "white", "red", "cyan", "green", "yellow", "purple", "orange", "maroon", "gray"];
+let contador = 0;
+
+// primer contenedor interior
+function cambiarVerde (event) {
+    let colorAleatorio = Math.floor(Math.random() * colores.length);
+    verde.style.backgroundColor = (colores[colorAleatorio]);
+    document.getElementById('textoVerde').style.backgroundColor = (colores[colorAleatorio]);
+
+    // getComputedStyle devuelve objeto con propiedades css del contenedor con id 'verde', accedepos a la propiedad backgoround y comparamos
+    if (getComputedStyle(verde).backgroundColor == 'rgb(0, 128, 0)') {
+        document.getElementById('textoVerde').textContent = '¡HECHO!';
+        verde.removeEventListener("click", cambiarVerde);
+    }
+    event.stopPropagation();
+}
+verde.addEventListener("click", cambiarVerde);
+
+// segundo contenedor interior
+function cambiarRojo (event) {
+    let colorAleatorio = Math.floor(Math.random() * colores.length);
+    rojo.style.backgroundColor = (colores[colorAleatorio]);
+    document.getElementById('textoRojo').style.backgroundColor = (colores[colorAleatorio]);
+
+    if (getComputedStyle(rojo).backgroundColor == 'rgb(255, 0, 0)') {
+        document.getElementById('textoRojo').textContent = '¡HECHO!';
+        rojo.removeEventListener("click", cambiarRojo);
+    }
+    event.stopPropagation();
+}
+rojo.addEventListener("click", cambiarRojo);
+
+// tercer contenedor interior
+function cambiarCyan(event) {
+    let colorAleatorio = Math.floor(Math.random() * colores.length);
+    cyan.style.backgroundColor = (colores[colorAleatorio]);
+    document.getElementById('textoCyan').style.backgroundColor = (colores[colorAleatorio]);
+
+    if (getComputedStyle(cyan).backgroundColor == 'rgb(0, 255, 255)') {
+        document.getElementById('textoCyan').textContent = '¡HECHO!';
+        cyan.removeEventListener("click", cambiarCyan);
+    }
+    event.stopPropagation();
+}
+cyan.addEventListener("click", cambiarCyan);
+
+// cuarto contenedor interior
+function cambiarNaranja(event) {
+    let colorAleatorio = Math.floor(Math.random() * colores.length);
+    naranja.style.backgroundColor = (colores[colorAleatorio]);
+    document.getElementById('textoNaranja').style.backgroundColor = (colores[colorAleatorio]);
+
+    if (getComputedStyle(naranja).backgroundColor == 'rgb(255, 165, 0)') {
+        document.getElementById('textoNaranja').textContent = '¡HECHO!';
+        naranja.removeEventListener("click", cambiarNaranja);
+    }
+    event.stopPropagation();
+}
+naranja.addEventListener("click", cambiarNaranja);
