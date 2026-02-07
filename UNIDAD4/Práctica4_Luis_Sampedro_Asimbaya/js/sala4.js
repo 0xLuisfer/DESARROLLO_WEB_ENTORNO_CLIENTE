c69dc5cbeb73fc1f40a7d394c251e98e480bc75f
Luisfer <lfernans0210@gmail.com>
2026-02-07 17:01:55 +0100

    Práctica Unidad 5 (primera entrega): Se añade segundo evento donde se valida el uso y combinación de teclas especiales (CTRL + M)

--- UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala4.js
+++ UNIDAD4/Práctica4_Luis_Sampedro_Asimbaya/js/sala4.js
@@ -1,6 +1,6 @@
 // evento primer contenedor
-let contenedor1 = document.getElementById('cuadrado1');
-contenedor1.addEventListener("mousedown", (event) => {
+let contenedor1 = document.getElementById('contenedor1');
+contenedor1.addEventListener("mousedown", () => {
     const posicion = contenedor1.getBoundingClientRect();
     if ((posicion.x >= 345 && posicion.x <= 350) && (posicion.y >= 80 && posicion.y <= 85)) {
         alert(`¡Coordenadas correctas! x: ${posicion.x}, y: ${posicion.y}`);
@@ -11,3 +11,20 @@ contenedor1.addEventListener("mousedown", (event) => {
         alert(`Coordenadas incorrectas: x: ${posicion.x}, y: ${posicion.y}`)
     }
 });
+
+let contenedor2 = document.getElementById('contenedor2');
+
+function ctrlM(event) {
+    if (event.ctrlKey && event.code === "KeyM") {
+        alert("Correcta la combinación de teclas CTRL + M");
+        event.preventDefault(); // evitamos cualquier acción por defecto de esta combinacin de teclas
+
+        contenedor2.textContent = "¡CORRECTO!";
+        contenedor2.style.backgroundColor = "green";
+
+        document.removeEventListener("keydown", ctrlM); // eliminamos el evento después de que el usuario lo haya ejecutado
+    }
+}
+
+// disparamos el evento CTRL + M
+document.addEventListener("keydown", ctrlM);
